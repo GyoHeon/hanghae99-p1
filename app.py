5db34ec441ef3aac6a05f7ae0f36f352c3e30346
jonghoon1978 <86106738+jonghoon1978@users.noreply.github.com>
2021-11-04 19:06:37 +0900

    챌린지참가유저별 UI

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-from datetime import datetime
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -13,24 +12,21 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-
 client = MongoClient('localhost', 27017)
-#client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
-#client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+# client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbproject1
 
 
-now = datetime.now()
-date_time = now.strftime("%Y-%m-%d-%H:%M")
 # 메인페이지-챌린지 정보 주기__이교헌
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"username":payload["id"]})
+        user_info = db.users.find_one({"username": payload["id"]})
         challenges = list(db.chall.find({}, {"_id": False}))
-        return render_template('index.html', user_info=user_info, challenges = challenges)
+        return render_template('index.html', user_info=user_info, challenges=challenges)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -54,8 +50,8 @@ def sign_in():
     print(result)
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60)  # 로그인 1시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60)  # 로그인 1시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -65,20 +61,20 @@ def sign_in():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-#회원가입 - 이교헌
+# 회원가입 - 이교헌
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": "",                                         # 프로필 한 마디
-        "profile_chall": []                                         # 참가한 챌린지
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": "",  # 프로필 한 마디
+        "profile_chall": []  # 참가한 챌린지
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -92,13 +88,19 @@ def check_dup():
     return jsonify({'result': 'success', 'exists': exists})
 
 
-# 상세페이지이동
+# 상세페이지이동 - 이한울
 @app.route('/detail/<title_give>')
 def detail(title_give):
-    challenge = db.chall.find_one({"title": title_give}, {"_id": False})
-    img = challenge["url"]
-    desc = challenge["description"]
-    return render_template('detail.html', title=title_give, img=img, desc=desc, date_time=date_time)
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_challenges = db.users.find_one({"username": payload["id"]})["profile_chall"]
+        challenge = db.chall.find_one({"title": title_give}, {"_id": False})
+        img = challenge["url"]
+        desc = challenge["description"]
+        return render_template('detail.html', title=title_give, img=img, desc=desc, user_challenges=user_challenges)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
 
 # 상세페이지 인증글 db에 저장-이한울
@@ -122,9 +124,8 @@ def posting():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-      
+
 # 상세페이지 참가 db에 저장 - 이한울
-# insert 고쳐야함 - date 필요한지 재검토
 @app.route('/my_chall', methods=['POST'])
 def my_chall():
     token_receive = request.cookies.get('mytoken')
@@ -132,30 +133,21 @@ def my_chall():
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.users.find_one({"username": payload["id"]})
         profile_chall_receive = request.form["profile_chall_give"]
-        date_receive = request.form["date_give"]
-        doc = {
-            "username": user_info["username"],
-            "profile_chall": profile_chall_receive,
-            "date": date_receive
-        }
-        db.users.insert_one(doc)
-        db.users.update_one({'username':user_info["username"]},{'$push':{'profile_chall':profile_chall_receive}})
-
+        db.users.update_one({'username': user_info["username"]}, {'$push': {'profile_chall': profile_chall_receive}})
         return jsonify({"result": "success", 'msg': '포스팅 성공'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-      
-      
+
+
 '''
 # 뱃지 시스템 - 이교헌
 @app.route('/my_badges', method=['GET'])
 def badge():
-    all_day = request.from['days_give']
+    all_day = request.form['days_give']
     #이거 우짬
     now_day = db.users.
     progress = now_day//all_day
 '''
 
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
