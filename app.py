79f4d648875bca619ea3f76e2310ba409142a6e0
GyoHeon Lee <ghl12078@gmail.com>
2021-11-06 13:07:27 +0900

    add login function again

--- app.py
+++ app.py
@@ -37,6 +37,10 @@ def home():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
 
 # 로그인 - 이교헌
 @app.route('/sign_in', methods=['POST'])
@@ -219,4 +223,4 @@ def save_img():
       
       
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=8000, debug=True)
